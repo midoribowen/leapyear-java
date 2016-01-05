913fa9689993f6ec2c706d1372f36b79818bc17b
Anitha Srinivasan and Midori Bowen <anitha+midori@>
2016-01-04 16:58:29 -0800

    unfinished leetSpeak.

--- leet-speak/src/main/java/LeetSpeak.java
+++ leet-speak/src/main/java/LeetSpeak.java
@@ -1,5 +1,19 @@
 public class LeetSpeak {
   public static void main(String[] args) {
-      
+
+    public String leetSpeak(String userInput) {
+      String returnString = new String();
+
+      for ( int i = 0; i < str.length(); i++ ) {
+        if ( userInput.charAt(i) == 'e' || 'E') {
+          returnString += "3";
+        } else {
+          returnString += userInput.charAt(i);
+        }
+        }
+      }
+      return userInput.charAt();
+    }
+
   }
 }
--- leet-speak/src/test/java/LeetSpeakTest.java
+++ leet-speak/src/test/java/LeetSpeakTest.java
@@ -2,5 +2,17 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class LeetSpeakTest {
-  
+
+  @Test
+  public void leetSpeak_returnsAStringAsIsWhenNoLeetSpeakIsPresent_wordDoesntChange() {
+  leetSpeak userInput = new leetSpeak();
+  assertEquals("happy", userInput.leetSpeak("happy"));
+    }
+    
+    @Test
+  public void leetspeak_replacesEveryEInAStringWithA3_wordChanges() {
+    AppName yourApp = new AppName();
+    assertEquals("3l3phant", yourApp.leetspeak("elephant"));
+  }
+
 }
